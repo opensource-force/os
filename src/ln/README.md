522ac83ddfd855fb698120c8129e1d3a43396313
wick3dr0se <wick3dr0se@protonmail.com>
2024-01-26 04:29:28 -0500

    allow first & second form of `ln`; fallback to current working directory

--- src/ln/README.md
+++ src/ln/README.md
@@ -1,7 +1,19 @@
 # ln
 
-Make hard/symbolic links
+Make links between files
 
 ```sh
-ln [OPTION]... <TARGET>... [DESTINATION]
+ln [OPTION]... <TARGET> <LINK_NAME>
+ln [OPTION]... <TARGET>
+#ln [OPTION]... <TARGET>... <DIRECTORY>
 ```
+
+## Description
+First form - Create a link to TARGET with the name LINK_NAME
+
+Second form - Create a link to TARGET in the current directory
+
+~~Third & Foruth form - Create links to each TARGET in DIRECTORY~~
+
+## Options
+`-s`, `--symbolic`  ..  make symbolic links
--- src/ln/ln.rs
+++ src/ln/ln.rs
@@ -1,30 +1,35 @@
-// https://man.archlinux.org/man/ln.1
-
 use std::fs;
 use std::os::unix::fs as unix_fs;
+use std::path::Path;
 
 fn main() {
     let mut opts = clop::get_opts();
-    
-    if opts.scrap.len() == 2 {
-        if opts.has(&["s", "symbolic"], None) {
-            let og = &opts.scrap[0];
-            let link = &opts.scrap[1];
+    let is_symbolic = opts.has(&["s", "symbolic"], None);
+    let og: &String;
+    let link: String;
 
-            match unix_fs::symlink(og, link) {
-                Ok(_) => println!("Created symlink {}", link),
-                Err(e) => println!("Error creating symlink {}", e)
-            }
-        } else {
-            let og = &opts.scrap[0];
-            let link = &opts.scrap[1];
-            
-            match fs::hard_link(og, link) {
-                Ok(_) => println!("Created hardlink {}", link),
-                Err(e) => println!("Error creating hardlink {}", e)
-            }
-        }
+    if opts.scrap.len() == 2 {
+        og = &opts.scrap[0];
+        link = opts.scrap[1].to_string();
+    }
+    else if opts.scrap.len() == 1 {
+        og = &opts.scrap[0];
+        let filename = Path::new(og).file_name().unwrap().to_string_lossy();
+        link = format!("./{}", filename);
     } else {
         println!("Incorrect number of arguments");
+        return;
+    }
+
+    if is_symbolic {
+        match unix_fs::symlink(og, &link) {
+            Ok(_) => println!("Created symlink {}", link),
+            Err(e) => println!("Error creating symlink {}", e)
+        }
+    } else {
+        match fs::hard_link(og, &link) {
+            Ok(_) => println!("Created hardlink {}", link),
+            Err(e) => println!("Error creating hardlink {}", e)
+        }
     }
 }
