bcde95d083080254e53d7cf57733bd8880d87540
wick3dr0se <wick3dr0se@protonmail.com>
2024-01-26 14:00:14 -0500

    seperate create_link function & add test

--- src/ln/ln.rs
+++ src/ln/ln.rs
@@ -1,35 +1,69 @@
 use std::fs;
 use std::os::unix::fs as unix_fs;
 use std::path::Path;
+use std::process;
+
+fn create_link(target_path: &str, link_path: &str, is_symbolic: bool) {
+    if is_symbolic {
+        match unix_fs::symlink(target_path, &link_path) {
+            Ok(_) => println!("Created symlink {}", link_path),
+            Err(e) => {
+                eprintln!("Error creating symlink {}", e);
+                process::exit(1);
+            }
+        }
+    } else {
+        match fs::hard_link(target_path, &link_path) {
+            Ok(_) => println!("Created hardlink {}", link_path),
+            Err(e) => {
+                eprintln!("Error creating hardlink {}", e);
+                process::exit(1);
+            }
+        }
+    }
+}
 
 fn main() {
     let mut opts = clop::get_opts();
     let is_symbolic = opts.has(&["s", "symbolic"], None);
-    let og: &String;
-    let link: String;
 
-    if opts.scrap.len() == 2 {
-        og = &opts.scrap[0];
-        link = opts.scrap[1].to_string();
-    }
-    else if opts.scrap.len() == 1 {
-        og = &opts.scrap[0];
-        let filename = Path::new(og).file_name().unwrap().to_string_lossy();
-        link = format!("./{}", filename);
-    } else {
-        println!("Incorrect number of arguments");
-        return;
+    let (target_path, link_path): (&String, String) = match opts.scrap.len() {
+        2 => (&opts.scrap[0], opts.scrap[1].to_string()),
+        1 => {
+            let link_name = Path::new(&opts.scrap[0]).file_name().unwrap_or_default().to_string_lossy().to_string();
+            (&opts.scrap[0], link_name)
+        },
+        _ => {
+            eprintln!("Incorrect number of arguments");
+            process::exit(1);
+        }
+    };
+
+    create_link(target_path, &link_path, is_symbolic);
+}
+
+#[cfg(test)]
+mod tests {
+    use fs;
+    use create_link;
+
+    #[test]
+    fn test_hardlink() {
+        let _ = fs::File::create("a");
+
+        create_link("a", "b", false);
+
+        let _ = fs::remove_file("a");
+        let _ = fs::remove_file("b");
     }
 
-    if is_symbolic {
-        match unix_fs::symlink(og, &link) {
-            Ok(_) => println!("Created symlink {}", link),
-            Err(e) => println!("Error creating symlink {}", e)
-        }
-    } else {
-        match fs::hard_link(og, &link) {
-            Ok(_) => println!("Created hardlink {}", link),
-            Err(e) => println!("Error creating hardlink {}", e)
-        }
+    #[test]
+    fn test_symlink() {
+        let _ = fs::File::create("a");
+
+        create_link("a", "b", true);
+
+        let _ = fs::remove_file("a");
+        let _ = fs::remove_file("b");
     }
 }
