775f6e1eb9427b6aafbdb6f3851a71151238ec54
wick3dr0se <wick3dr0se@protonmail.com>
2024-02-03 02:56:45 -0500

    make is_recursive not optional; refactor

--- src/rm/rm.rs
+++ src/rm/rm.rs
@@ -2,33 +2,28 @@
 
 use std::fs;
 
-fn remove(file: &str, is_recursive: Option<bool>) -> bool {
-    let mut file_type = "file";
-    let result = match is_recursive {
-        Some(true) => {
-            match fs::metadata(file) {
-                Ok(meta) => {
-                    if meta.is_file() {
-                        fs::remove_file(file)
-                    } else {
-                        file_type = "directory";
-                        fs::remove_dir_all(file)
-                    }
-                },
-                Err(_) => return false
-            }
-        },
-        None | Some(false) => fs::remove_file(file)
+fn remove(file: &str, is_recursive: bool) -> bool {
+    let (file_type, result) = if is_recursive {
+        match fs::metadata(file) {
+            Ok(meta) => {
+                if meta.is_dir() {
+                    ("directory", fs::remove_dir_all(file))
+                } else {
+                    ("file", fs::remove_file(file))
+                }
+            },
+            Err(_) => return false
+        }
+    } else {
+        ("file", fs::remove_file(file))
     };
 
-    match result {
-        Ok(_) => println!("Removed {}: {}", file_type, file),
-        Err(e) => {
-            eprintln!("Error removing {}: {}", file_type, e);
-            return false
-        }
+    if let Err(e) = result {
+        eprintln!("Error removing {}: {}", file_type, e);
+        return false
     }
 
+    println!("Removed {}: {}", file_type, file);
     return true
 }
 
@@ -43,18 +38,19 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use fs;
-    use remove;
+    use super::*;
 
     #[test]
-    fn remove_file() {
+    fn test_remove_file() {
         let _ = fs::File::create("a");
+
         assert!(remove("a", Some(false)));
     }
 
     #[test]
-    fn remove_dir() {
+    fn test_remove_dir() {
         let _ = fs::create_dir("b");
+        
         assert!(remove("b", Some(true)));
     }
 }
