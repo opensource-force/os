b507b88c8d209c6cf1d1d17a21e06e4b1ca3529c
wick3dr0se <wick3dr0se@protonmail.com>
2024-02-03 02:49:40 -0500

    seperate list_dir, implement hidden files with -a & test

--- src/ls/ls.rs
+++ src/ls/ls.rs
@@ -1,31 +1,53 @@
 // https://man.archlinux.org/man/ls.1.en
-// -a | show hidden files
 
-use std::env;
 use std::fs;
+
+fn list_dir(dst: &str, is_hidden: bool) -> bool {
+    match fs::read_dir(dst) {
+        Ok(paths) => {
+            for path in paths {
+                if let Ok(file) = path.unwrap().path().strip_prefix(dst) {
+                    let file_name = file.file_name().unwrap().to_string_lossy();
+                  
+                    if is_hidden || !file_name.starts_with(".") {
+                        println!("{}", file.display());
+                    }
+                }
+            }
+        },
+        Err(e) => {
+            eprintln!("Cannot access '{}': {}", dst, e);
+            return false
+        }
+    }
+
+    return true
+}
 
 fn main() {
     let mut opts = clop::get_opts();
+    let is_hidden = opts.has(&["a", "all"], None);
 
     if opts.scrap.len() == 0 {
-        let home = env::var("HOME").unwrap();
-        opts.scrap.push(home);
+        opts.scrap.push("./".to_string());
     }
 
     for arg in &opts.scrap {
-        match fs::read_dir(arg) {
-            Ok(paths) => {
-                for path in paths {
-                    if let Ok(file) = path.unwrap().path().strip_prefix(arg) {
-                        let file_name = file.file_name().unwrap().to_string_lossy();
-                      
-                        if !file_name.starts_with(".") {
-                            println!("{}", file.display());
-                        }
-                    }
-                }
-            },
-            Err(e) => println!("Error reading {}: {}", arg, e)
-        }
+        list_dir(arg, is_hidden);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_list_dir() {
+        assert!(list_dir("./", false));
+    }
+
+    #[test]
+    fn test_list_dir_hidden() {
+        assert!(list_dir("./", true));
     }
 }
