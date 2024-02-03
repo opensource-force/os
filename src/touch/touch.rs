0085f7d1b28c63d3e1c524cad29f71e5da33d4db
wick3dr0se <wick3dr0se@protonmail.com>
2024-02-03 02:57:15 -0500

    seperate make_file & implement test

--- src/touch/touch.rs
+++ src/touch/touch.rs
@@ -2,17 +2,47 @@
 
 use std::fs;
 
+fn make_file(file: &str) -> bool {
+    match fs::File::create(file) {
+        Ok(created) => {
+            println!("Created: {}", file);
+    
+            let _ = created.set_len(0);
+        },
+        Err(e) => {
+            eprintln!("Error creating {}: {}", file, e);
+            return false
+        }
+    }
+
+    return true
+}
+
 fn main() {
     let opts = clop::get_opts();
     
     for arg in &opts.scrap {
-        match fs::File::create(arg) {
-            Ok(file) => {
-                println!("Created {}", arg);
-        
-                let _ = file.set_len(0);
-            },
-            Err(e) => println!("Error creating {}: {}", arg, e)
-        }
+        make_file(arg);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_make_file() {
+        assert!(make_file("a"));
+
+        let _ = fs::remove_file("a");
+    }
+
+    #[test]
+    fn test_update_timestamp() {
+        let _ = fs::File::create("b");
+
+        assert!(make_file("b"));
+
+        let _ = fs::remove_file("b");
     }
 }
