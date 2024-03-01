c6ba65d99a004fa665f592824d840136b5f95885
wick3dr0se <wick3dr0se@protonmail.com>
2024-03-01 06:37:42 -0500

    write to screen

--- src/kernel/README.md
+++ src/kernel/README.md
@@ -10,10 +10,10 @@ Build binary from target
 `cargo build --target x86_64-unknown-uefi -Z build-std=core -Z build-std-features=compiler-builtins-mem`
 
 Create disk image
-`cargo run --package disk_image -- ../target/x86_64-unknown-uefi/debug/kernel.efi`
+`cargo run --package disk_image -- target/x86_64-unknown-uefi/debug/kernel.efi`
 
 Boot with QEMU
-`qemu-system-x86_64 -drive format=raw,file=../target/x86_64-unknown-uefi/debug/kernel.gdt -bios OVMF_CODE-pure-efi.fd`
+`qemu-system-x86_64 -drive format=raw,file=target/x86_64-unknown-uefi/debug/kernel.gdt -bios OVMF_CODE-pure-efi.fd`
 
 ## Specification
 - [WASI](https://github.com/WebAssembly/WASI) - ABI
--- src/kernel/kernel.rs
+++ src/kernel/kernel.rs
@@ -1,12 +1,12 @@
 // https://github.com/phil-opp/blog_os/blob/edition-3/blog/content/edition-3/posts/02-booting/uefi/index.md
 
-//#![feature(abi_efiapi)]
-
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
-use core::ffi::c_void;
+use core::fmt::Write;
+
+use uefi::prelude::entry;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! { loop {} }
@@ -14,9 +14,15 @@ fn panic(_info: &PanicInfo) -> ! { loop {} }
 #[no_mangle]
 pub extern "C" fn _start() -> ! { loop {} }
 
-#[no_mangle]
-pub extern "efiapi" fn efi_main(
-    image: *mut c_void,
-    system_table: *const c_void,
-) -> usize
-{ loop {} }
+
+#[entry]
+fn efi_main(
+    image: uefi::Handle,
+    mut system_table: uefi::table::SystemTable<uefi::table::Boot>,
+) -> uefi::Status {
+    let stdout = system_table.stdout();
+    
+    writeln!(stdout, "Hello World!").unwrap();
+
+    loop {}
+}
