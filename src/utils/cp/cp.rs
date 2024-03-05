b72f117f789aeddb51b56eb1e9d54d310650bffa
FssAy <49795742+FssAy@users.noreply.github.com>
2024-03-05 16:45:49 +0000

    improve `cp` - Patch (#2)
    * improve `cp`
    
    * refactor `cp` functions to accept generic path types
    
    * move custom `cp` io functions into a dedicated module

--- src/utils/cp/cp.rs
+++ src/utils/cp/cp.rs
@@ -1,44 +1,14 @@
 // https://man.archlinux.org/man/cp.1
 
+mod io;
+
 use std::borrow::Cow;
 use std::error::Error;
 use std::fs;
 use std::path::Path;
 
-fn copy_file(src_path: Cow<Path>, dst_path: Cow<Path>) -> Result<(), Box<dyn Error>> {
-    fs::copy(&src_path, &dst_path)?;
-    println!("Copied '{}' to '{}'", src_path.display(), dst_path.display());
-    Ok(())
-}
-
-fn copy_directory(src_path: Cow<Path>, dst_path: Cow<Path>, is_recursive: bool) -> Result<(), Box<dyn Error>> {
-    fs::create_dir_all(&dst_path)?;
-
-    for entry in fs::read_dir(&src_path)? {
-        let entry_path = Cow::from(
-            entry?.path()
-        );
-
-        let new_dst = match entry_path.file_name() {
-            Some(file_name) => Cow::from(dst_path.join(file_name)),
-            None => dst_path.clone(),  // this should never happen
-        };
-
-        if entry_path.is_file() {
-            copy_file(entry_path, new_dst)?;
-        } else if entry_path.is_dir() && is_recursive {
-            copy_directory(entry_path, new_dst, is_recursive)?;
-        } else {
-            // invalid path (shouldn't happen as it's a result of fs::read_dir, but I am not sure)
-            // My suggestion is to ignore this case and print a warning.
-        }
-    }
-
-    Ok(())
-}
-
 fn cp(src: &str, dst: &str, is_recursive: bool) -> Result<(), Box<dyn Error>> {
-    let src_path = Cow::from(Path::new(src));
+    let src_path = Path::new(src);
     let mut dst_path = Cow::from(Path::new(dst));
 
     // ToDo: Handle cases when passing invalid paths and add some errors on '// invalid path'.
@@ -50,14 +20,13 @@ fn cp(src: &str, dst: &str, is_recursive: bool) -> Result<(), Box<dyn Error>> {
     // so I am leaving it for @wick3dr0se to decide and implement
 
     if dst_path.is_dir() {
-        dst_path.to_mut().push(&src_path);
+        dst_path.to_mut().push(src_path);
     }
 
     if src_path.is_dir() {
-        dst_path.to_mut().push(&src_path);
-        copy_directory(src_path, dst_path, is_recursive)
+        io::copy_directory(src_path, dst_path, is_recursive)
     } else if src_path.is_file() {
-        copy_file(src_path, dst_path)
+        io::copy_file(src_path, dst_path)
     } else {
         // invalid path - src_path doesn't exist
         Ok(())
