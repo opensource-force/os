8c4ac02c05ff19885b4963f03af4c8c4079a3f2d
wick3dr0se <wick3dr0se@protonmail.com>
2024-03-31 01:58:05 -0500

    rewrite shell; include toml

--- src/shell/shell.rs
+++ src/shell/shell.rs
@@ -1,24 +1,14 @@
-use std::error::Error;
+pub(crate) mod lex;
+pub(crate) mod parser;
+
 use std::io::{self, Write};
-use std::{fs, env};
+use lex::Lexer;
+use parser::{Parser, ParsedTk};
 
-use lex::{TkType, Lexer};
 
-const AMBER: &str = "38;2;255;176;0m";
-const NEON: &str = "38;2;102;255;102m";
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let _home: &str = &env::var("HOME")?;
-    let user: &str = &env::var("USER")?;
-    let host = fs::read_to_string("/etc/hostname")?;
-
-    //print!("\x1B[2J\x1B[3J\x1B[H");
-
+fn main() {
     loop {
-        let cwd = env::current_dir()?;
-
-        print!("\x1B[{}{}\x1B[m\n{}\x1B[2m@{}\x1B[m \x1B[{}>\x1B[m: ",
-            AMBER, cwd.to_string_lossy(), user, host.trim(), NEON);
+        print!("> ");
         io::stdout().flush().unwrap();
 
         let mut buf = String::new();
@@ -28,20 +18,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         lexer.src.input.extend(buf.chars());
         lexer.lex();
 
-        while let Some(token) = lexer.tokens.next() {
-            match token.tk_type {
-                TkType::Pipe => println!("Found Pipe"),
-                TkType::Lexeme => {
-                    match token.lexeme.as_str() {
-                        "exit" => {
-                            println!("Shell closed!");
-                            return Ok(())
-                        },
-                        _ => println!("Input not parsed")
-                    }
-                }
-            _ => {} // implement input/output redirection
-            }
-        }
+        let mut parser = Parser::new(lexer);
+        let tokens = parser.parse();
+
+        println!("{:?}", tokens);
     }
 }
