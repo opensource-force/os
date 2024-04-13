daa8a1833dd5595d83c894298bc35e82d9079931
wick3dr0se <wick3dr0se@protonmail.com>
2024-04-13 16:00:48 -0500

    set errtrap on; cleanup

--- src/kernel/qemu.sh
+++ src/kernel/qemu.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-cargo build --target x86_64-unknown-uefi -Z build-std=core -Z build-std-features=compiler-builtins-mem
-cargo run --package disk_image -- target/x86_64-unknown-uefi/debug/kernel.efi
-qemu-system-x86_64 -drive format=raw,file=target/x86_64-unknown-uefi/debug/kernel.gdt -bios OVMF_CODE-pure-efi.fd
+set -e
+
+buildDir='target/x86_64-unknown-uefi/debug'
+
+cargo build \
+    --target x86_64-unknown-uefi \
+    -Z build-std=core \
+    -Z build-std-features=compiler-builtins-mem
+
+cargo run \
+    --package disk_image \
+    -- "$buildDir/kernel.efi"
+
+qemu-system-x86_64 \
+    -drive format=raw,file="$buildDir/kernel.gdt" \
+    -bios OVMF_CODE-pure-efi.fd
