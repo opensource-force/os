74843164ffec95d4d13541a88e53a87d718784cf
wick3dr0se <wick3dr0se@protonmail.com>
2024-04-15 18:28:55 -0500

    locate ACPI RSDP tables using UEFI config table; print RSDP address

--- src/kernel/kernel.rs
+++ src/kernel/kernel.rs
@@ -8,7 +8,10 @@ extern crate alloc;
 use alloc::alloc::Layout;
 use alloc::vec::Vec;
 
-use uefi::prelude::*;
+use uefi::{
+    prelude::*,
+    table::cfg
+};
 use uefi_services::{ init, println };
 
 #[entry]
@@ -34,6 +37,17 @@ fn efi_main(
     println!("firmware vendor: {}", firmware_vendor);
     println!("firmware revision: {}", firmware_rev);
     println!("UEFI revision: {}.{}", uefi_rev_major, uefi_rev_minor);
+
+    // locating ACPI RSDP tables with UEFI config table
+    // Section 4.6: https://uefi.org/sites/default/files/resources/UEFI%20Spec%202.8B%20May%202020.pdf
+    // Section 5.2.5.2: https://uefi.org/sites/default/files/resources/ACPI_6_2.pdf
+    // may remove for bootloader crate
+    let mut config_entries = sys_table.config_table().iter();
+    let rsdp_addr = config_entries
+        .find(|entry| matches!(entry.guid, cfg::ACPI_GUID | cfg::ACPI2_GUID))
+        .map(|entry| entry.address);
+
+    println!("RSDP addr: {:?}", rsdp_addr);
 
     // pause 10s
     //sys_table.boot_services().stall(10_000_000);
